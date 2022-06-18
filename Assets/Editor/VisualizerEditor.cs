e0f51b04b87663797f259608141b0ddcb834e85f
Simsimhan Chobo <jun32820@gmail.com>
2022-06-18 18:23:05 +0900

    에디터 스크립트 추가

---
+++ Assets/Editor/VisualizerEditor.cs
@@ -0,0 +1,48 @@
+using SCKRM.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace SDJK.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(Visualizer))]
+    public class VisualizerEditor : CustomInspectorEditor
+    {
+        Visualizer editor;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            editor = (Visualizer)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            UseProperty("barPrefab", "바 프리팹");
+
+            Space();
+
+            UseProperty("_all", "한번에 변경");
+
+            Space();
+
+            if (!editor.all)
+            {
+                UseProperty("_left", "왼쪽으로 애니메이션");
+                UseProperty("_divide", "분할");
+
+                Space();
+            }
+
+            UseProperty("_alpha");
+            UseProperty("_offset");
+            UseProperty("_size");
+
+            Space();
+
+            UseProperty("_length");
+        }
+    }
+}
