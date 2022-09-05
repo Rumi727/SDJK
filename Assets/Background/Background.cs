79823c58c6d0b18b5e64247e84e6c1a2afbd8865
SimsimhanChobo <jun32820@gmail.com>
2022-09-05 22:44:50 +0900

    이미지 로드 메소드 비동기로 변경

--- Assets/Background/Background.cs
+++ Assets/Background/Background.cs
@@ -4,11 +4,11 @@ using SCKRM.Resource;
 using SCKRM.Rhythm;
 using SCKRM.UI;
 using SDJK.Map;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
 
 namespace SDJK
@@ -29,36 +29,45 @@ namespace SDJK
 
             transform.SetSiblingIndex(0);
             map = MapManager.selectedMap;
+
+            TextureLoad().Forget();
         }
 
         string tempTexturePath = "";
+        Dictionary<string, Sprite> loadedSprites = new Dictionary<string, Sprite>();
         void Update()
         {
             if (!padeOut)
             {
-                canvasGroup.alpha = canvasGroup.alpha.MoveTowards(1, 0.05f * Kernel.fpsUnscaledDeltaTime);
-
+                if (loadedSprites.Count > 0)
                 {
-                    string texturePath = map.globalEffect.background.GetValue(RhythmManager.currentBeatScreen).backgroundFile;
-                    if (texturePath != tempTexturePath)
+                    canvasGroup.alpha = canvasGroup.alpha.MoveTowards(1, 0.05f * Kernel.fpsUnscaledDeltaTime);
+
                     {
-                        tempTexturePath = texturePath;
-
-                        if (image.sprite != null)
+                        DateTime now = DateTime.Now;
+                        string texturePath;
+                        if (now.Hour >= 0 && now.Hour < 4)
                         {
-                            Destroy(image.sprite.texture);
-                            Destroy(image.sprite);
+                            texturePath = map.globalEffect.background.GetValue(RhythmManager.currentBeatScreen).backgroundNightFile;
+
+                            if (string.IsNullOrEmpty(texturePath))
+                                texturePath = map.globalEffect.background.GetValue(RhythmManager.currentBeatScreen).backgroundFile;
                         }
+                        else
+                            texturePath = map.globalEffect.background.GetValue(RhythmManager.currentBeatScreen).backgroundFile;
 
-                        texturePath = PathTool.Combine(map.mapFilePathParent, texturePath);
-                        image.sprite = ResourceManager.GetSprite(ResourceManager.GetTexture(texturePath, false, FilterMode.Bilinear, true, TextureMetaData.CompressionType.none));
+                        if (texturePath != tempTexturePath)
+                        {
+                            tempTexturePath = texturePath;
+                            TextureChange(texturePath).Forget();
+                        }
                     }
-                }
 
-                if (image.sprite != null)
-                    image.color = map.globalEffect.backgroundColor.GetValue(RhythmManager.currentBeatScreen);
-                else
-                    image.color = Color.black;
+                    if (image.sprite != null)
+                        image.color = map.globalEffect.backgroundColor.GetValue(RhythmManager.currentBeatScreen);
+                    else
+                        image.color = Color.black;
+                }
             }
             else
             {
@@ -67,6 +76,34 @@ namespace SDJK
                 if (canvasGroup.alpha <= 0)
                     Remove();
             }
+        }
+
+        
+        async UniTaskVoid TextureLoad()
+        {
+            for (int i = 0; i < map.globalEffect.background.Count; i++)
+            {
+                BackgroundEffect backgroundEffect = map.globalEffect.background[i].value;
+                string background = backgroundEffect.backgroundFile;
+                string backgroundNight = backgroundEffect.backgroundNightFile;
+
+                string texturePath = PathTool.Combine(map.mapFilePathParent, background);
+                Texture2D texture = await ResourceManager.GetTextureAsync(texturePath, false, FilterMode.Bilinear, true, TextureMetaData.CompressionType.none);
+
+                string nightTexturePath = PathTool.Combine(map.mapFilePathParent, backgroundNight);
+                Texture2D nightTexture = await ResourceManager.GetTextureAsync(nightTexturePath, false, FilterMode.Bilinear, true, TextureMetaData.CompressionType.none);
+
+                if (texture != null && !loadedSprites.ContainsKey(background))
+                    loadedSprites.Add(background, ResourceManager.GetSprite(texture));
+                if (nightTexture != null && !loadedSprites.ContainsKey(backgroundNight))
+                    loadedSprites.Add(backgroundNight, ResourceManager.GetSprite(nightTexture));
+            }
+        }
+
+        async UniTaskVoid TextureChange(string texturePath)
+        {
+            await UniTask.WaitUntil(() => loadedSprites.ContainsKey(texturePath));
+            image.sprite = loadedSprites[texturePath];
         }
 
         public override bool Remove()
@@ -89,11 +126,13 @@ namespace SDJK
             cancelSource.Dispose();
             cancelSource = new CancellationTokenSource();
 
-            if (image.sprite != null)
+            foreach (var item in loadedSprites)
             {
-                Destroy(image.sprite.texture);
-                Destroy(image.sprite);
+                Destroy(item.Value.texture);
+                Destroy(item.Value);
             }
+
+            loadedSprites.Clear();
         }
     }
 }
