6567d0b2a42de852f1b12e3ca849d6d4120220b8
SimsimhanChobo <jun32820@gmail.com>
2022-10-28 00:08:21 +0900

    읽기 전용 해제

--- Assets/Global Effect/Effect.cs
+++ Assets/Global Effect/Effect.cs
@@ -6,7 +6,7 @@ namespace SDJK.Effect
 {
     public abstract class Effect : MonoBehaviour
     {
-        [SerializeField] EffectManager _effectManager; public EffectManager effectManager => _effectManager;
+        [SerializeField] EffectManager _effectManager; public EffectManager effectManager { get => _effectManager; set => _effectManager = value; }
 
         public Map.MapPack mapPack => effectManager.selectedMapPack;
         public Map.Map map => effectManager.selectedMap;
--- Assets/Global Effect/EffectManager.cs
+++ Assets/Global Effect/EffectManager.cs
@@ -1,5 +1,4 @@
 using SCKRM.Sound;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
