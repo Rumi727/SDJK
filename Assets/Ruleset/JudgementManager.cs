57c6afd101f3450ff0122ba7573c5c6610861426
Simsimhan Chobo <jun32820@gmail.com>
2022-12-06 02:58:01 +0900

    JudgementManager 추가

---
+++ Assets/Ruleset/JudgementManager.cs
@@ -0,0 +1,25 @@
+using SCKRM;
+using SDJK.Ruleset;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SDJK.Ruleset
+{
+    public static class JudgementManager
+    {
+        public static bool Judgement(this IRuleset ruleset, double disSecond, bool forceFastMiss, out JudgementMetaData judgementMetaData)
+        {
+            for (int i = 0; i < ruleset.judgementMetaDatas.Length; i++)
+            {
+                judgementMetaData = ruleset.judgementMetaDatas[i];
+
+                if (disSecond.Abs() < judgementMetaData.sizeSecond)
+                    return true;
+            }
+
+            judgementMetaData = ruleset.missJudgementMetaData;
+            return forceFastMiss || disSecond >= 0;
+        }
+    }
+}
