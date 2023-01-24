0e5abf582a3a5325f3eb2cd1c64249f019ea0c27
Simsimhan Chobo <jun32820@gmail.com>
2023-01-25 03:24:44 +0900

    주석 추가

--- Assets/Ruleset/SDJK/UI/SDJKUI.cs
+++ Assets/Ruleset/SDJK/UI/SDJKUI.cs
@@ -8,10 +8,16 @@ namespace SDJK.Ruleset.SDJK
     {
         [SerializeField, NotNull] SDJKJudgementManager _judgementManager; public SDJKJudgementManager judgementManager => _judgementManager;
 
+        /// <summary>
+        /// Please put base.OnEnable() when overriding
+        /// </summary>
         protected override void OnEnable() => judgementManager.judgementAction += JudgementAction;
 
         protected virtual void JudgementAction(double disSecond, bool isMiss, double accuracy, JudgementMetaData metaData) { }
 
+        /// <summary>
+        /// Please put base.OnDisable() when overriding
+        /// </summary>
         protected override void OnDisable() => judgementManager.judgementAction -= JudgementAction;
     }
 }
