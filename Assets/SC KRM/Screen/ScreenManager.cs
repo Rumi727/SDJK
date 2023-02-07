d1e5e2d51d6f1e80eb2275a71bfd22ead6913c00
Simsimhan Chobo <jun32820@gmail.com>
2023-02-08 00:17:01 +0900

    SC KRM 수정

--- Assets/SC KRM/Screen/ScreenManager.cs
+++ Assets/SC KRM/Screen/ScreenManager.cs
@@ -7,11 +7,47 @@ namespace SCKRM
     [WikiDescription("화면을 관리하는 클래스 입니다")]
     public sealed class ScreenManager : ManagerBase<ScreenManager>
     {
-        [WikiDescription("화면 폭")] public static int width { get; private set; }
-        [WikiDescription("화면 높이")] public static int height { get; private set; }
+        static int _width; [WikiDescription("화면 폭")] public static int width
+        {
+            get
+            {
+                if (Kernel.isPlaying)
+                    return _width;
+                else
+                    return Screen.width;
+            }
+        }
+        static int _height; [WikiDescription("화면 높이")] public static int height
+        {
+            get
+            {
+                if (Kernel.isPlaying)
+                    return _height;
+                else
+                    return Screen.height;
+            }
+        }
 
-        [WikiDescription("현재 모니터 해상도")] public static Resolution currentResolution { get; private set; }
-        [WikiDescription("현재 모니터가 지원하는 모든 전체화면 해상도")] public static Resolution[] resolutions { get; private set; }
+        static Resolution _currentResolution; [WikiDescription("현재 모니터 해상도")] public static Resolution currentResolution
+        {
+            get
+            {
+                if (Kernel.isPlaying)
+                    return _currentResolution;
+                else
+                    return Screen.currentResolution;
+            }
+        }
+        static Resolution[] _resolutions;  [WikiDescription("현재 모니터가 지원하는 모든 전체화면 해상도")] public static Resolution[] resolutions
+        {
+            get
+            {
+                if (Kernel.isPlaying)
+                    return _resolutions;
+                else
+                    return Screen.resolutions;
+            }
+        }
 
         void Awake()
         {
@@ -21,15 +57,15 @@ namespace SCKRM
 
         void Update()
         {
-            width = Screen.width;
-            height = Screen.height;
+            _width = Screen.width;
+            _height = Screen.height;
         }
 
         [WikiDescription("해상도 새로고침")]
         public static void ResolutionRefresh()
         {
-            currentResolution = Screen.currentResolution;
-            resolutions = Screen.resolutions;
+            _currentResolution = Screen.currentResolution;
+            _resolutions = Screen.resolutions;
         }
     }
 }
---
+++ Assets/SC KRM/UI/ResolutionLimit.cs
@@ -0,0 +1,37 @@
+using SCKRM.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SCKRM
+{
+    [ExecuteAlways, AddComponentMenu("SC KRM/UI/Resoluiton Limit"), RequireComponent(typeof(CanvasScaler))]
+    public sealed class ResolutionLimit : UI.UI
+    {
+        CanvasScaler _canvasScaler; public CanvasScaler canvasScaler => _canvasScaler = this.GetComponentFieldSave(_canvasScaler);
+
+        [SerializeField, Min(0)] Vector2 _min = Vector2.zero; public Vector2 min => _min;
+        [SerializeField, Min(0)] Vector2 _max = Vector2.zero; public Vector2 max => _max;
+
+        protected override void Awake() => canvasSetting.customGuiSize = true;
+
+        protected override void OnEnable() => Canvas.preWillRenderCanvases += Refresh;
+        protected override void OnDisable() => Canvas.preWillRenderCanvases -= Refresh;
+
+        void Refresh()
+        {
+            canvasSetting.customGuiSize = true;
+
+            float maxX = max.x;
+            float maxY = max.y;
+
+            if (maxX <= 0)
+                maxX = float.MaxValue;
+            if (maxY <= 0)
+                maxY = float.MaxValue;
+
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+            canvasScaler.referenceResolution = new Vector2((ScreenManager.width / UIManager.currentGuiSize).Clamp(min.x, maxX), (ScreenManager.height / UIManager.currentGuiSize).Clamp(min.y, maxY));
+        }
+    }
+}
