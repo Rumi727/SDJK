e600cfe576d290a1f72bbe2209d793a145b76716
Simsimhan Chobo <jun32820@gmail.com>
2023-02-18 04:49:31 +0900

    Update SDJKRuleset.cs

--- Assets/Ruleset/SDJK/SDJKRuleset.cs
+++ Assets/Ruleset/SDJK/SDJKRuleset.cs
@@ -60,7 +60,7 @@ namespace SDJK.Ruleset.SDJK
 
             base.GameStart(mapFilePath, replayFilePath, isEditor, modes);
 
-            await SceneManager.LoadScene("Assets/Ruleset/SDJK/SDJK.scene");
+            await SceneManager.LoadScene(3);
             await UniTask.NextFrame();
 
             SDJKReplayFile replay = null;
