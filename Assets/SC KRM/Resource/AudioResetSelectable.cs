470256ed03280896aeb518fcf6117bf9466b3995
Simsimhan Chobo <jun32820@gmail.com>
2023-03-12 16:25:39 +0900

    버그 수정

--- Assets/SC KRM/Resource/AudioResetSelectable.cs
+++ Assets/SC KRM/Resource/AudioResetSelectable.cs
@@ -4,10 +4,10 @@ using UnityEngine.UI;
 namespace SCKRM.Resource.UI
 {
     [WikiDescription("오디오 리셋 버튼을 관리하는 클래스 입니다")]
-    [AddComponentMenu("SC KRM/Resource/Audio Reset Button")]
-    public sealed class AudioResetButton : MonoBehaviour
+    [AddComponentMenu("SC KRM/Resource/Audio Reset Selectable")]
+    public sealed class AudioResetSelectable : MonoBehaviour
     {
-        [SerializeField] Button button;
+        [SerializeField] Selectable button;
 
         void Update() => button.interactable = !ResourceManager.audioResetProhibition;
     }
