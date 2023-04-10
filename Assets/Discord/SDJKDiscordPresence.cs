4cb827541e4e933e4b1440286c1966811daa9a07
Simsimhan Chobo <jun32820@gmail.com>
2023-04-10 21:59:30 +0900

    디스코드 게임 활동 추가

--- Assets/Discord/SDJKDiscordPresence.cs
+++ Assets/Discord/SDJKDiscordPresence.cs
@@ -1,14 +1,24 @@
 using SCKRM.Discord;
+using SCKRM.NTP;
 using SDJK.Effect;
+using System;
 using UnityEngine;
 
-namespace SDJK
+namespace SDJK.Discord
 {
     public sealed class SDJKDiscordPresence : MonoBehaviour
     {
         [SerializeField] EffectManager _effectManager;
         public EffectManager effectManager => _effectManager;
 
-        void Update() => DiscordManager.UpdateActivity();
+        long startTime = 0;
+        void Awake() => startTime = ((DateTimeOffset)NTPDateTime.utcNow).ToUnixTimeMilliseconds();
+        void Update()
+        {
+            if (effectManager == null || effectManager.selectedMap == null)
+                DiscordManager.UpdateActivity("Main Menu", null, null, null, null, null, startTime);
+            else
+                DiscordManager.UpdateActivity($"{effectManager.selectedMap.info.artist} - {effectManager.selectedMap.info.songName}", $"{effectManager.selectedMap.info.difficultyLabel}", null, null, null, null, startTime);
+        }
     }
 }
