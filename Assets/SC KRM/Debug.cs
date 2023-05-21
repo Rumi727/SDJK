6d280d43d4c26c3c438a4ef01ccbff92234d902f
Simsimhan Chobo <jun32820@gmail.com>
2023-05-21 15:38:14 +0900

    Update Debug.cs

--- Assets/SC KRM/Debug.cs
+++ Assets/SC KRM/Debug.cs
@@ -1,71 +1,61 @@
 using System;
 using System.Diagnostics;
 using System.Reflection;
+using JetBrains.Annotations;
 
 public static class Debug
 {
     [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
     public static void Log(object message, string className = null)
     {
-        if (className == null)
-            className = NameOfCallingClass();
-
+        className ??= NameOfCallingClass();
         UnityEngine.Debug.Log(LogText(className, message));
     }
 
     [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
     public static void LogWarning(object message, string className = null)
     {
-        if (className == null)
-            className = NameOfCallingClass();
-
+        className ??= NameOfCallingClass();
         UnityEngine.Debug.LogWarning(LogText(className, message));
     }
 
     [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
     public static void LogError(object message, string className = null)
     {
-        if (className == null)
-            className = NameOfCallingClass();
-
+        className ??= NameOfCallingClass();
         UnityEngine.Debug.LogError(LogText(className, message));
     }
 
     public static void ForceLog(object message, string className = null)
     {
-        if (className == null)
-            className = NameOfCallingClass();
-
+        className ??= NameOfCallingClass();
         UnityEngine.Debug.Log(ForceLogText(className, message));
     }
 
     public static void ForceLogWarning(object message, string className = null)
     {
-        if (className == null)
-            className = NameOfCallingClass();
-
+        className ??= NameOfCallingClass();
         UnityEngine.Debug.LogWarning(ForceLogText(className, message));
     }
 
     public static void ForceLogError(object message, string className = null)
     {
-        if (className == null)
-            className = NameOfCallingClass();
-
+        className ??= NameOfCallingClass();
         UnityEngine.Debug.LogError(ForceLogText(className, message));
     }
 
-    static string LogText(string className, object message) => "[" + className + "] " + message;
-    static string ForceLogText(string className, object message) => "<b>[" + className + "]</b> " + message;
+    static string LogText([NotNull] string className, object message) => "[" + className + "] " + message;
+    static string ForceLogText([NotNull] string className, object message) => "<b>[" + className + "]</b> " + message;
 
 
-    public static void LogException(Exception exception) => UnityEngine.Debug.LogException(exception);
+    public static void LogException([NotNull] Exception exception) => UnityEngine.Debug.LogException(exception);
 
     public static string NameOfCallingClass()
     {
         string name;
 
         Type declaringType;
+
         int skipFrames = 2;
         do
         {
