bb06ab88fa444fe7c8e57a832a9d239b5b0c6692
Simsimhan Chobo <jun32820@gmail.com>
2023-06-03 20:02:56 +0900

    비호환 모드 수정

--- Assets/Mode/Mode Base/Automatic/AutoModeBase.cs
+++ Assets/Mode/Mode Base/Automatic/AutoModeBase.cs
@@ -1,4 +1,6 @@
 using SCKRM.Renderer;
+using SDJK.Mode.Difficulty;
+using System;
 
 namespace SDJK.Mode.Automatic
 {
@@ -10,5 +12,7 @@ namespace SDJK.Mode.Automatic
         public override NameSpacePathPair info { get; } = "sdjk:mode.automatic.auto.info";
 
         public override NameSpaceIndexTypePathPair icon { get; } = "sdjk:0:mode/automatic_auto";
+
+        public override Type[] incompatibleModes { get; } = new Type[] { typeof(NoFailModeBase), typeof(SuddenDeathModeBase), typeof(PerfectModeBase), typeof(AutoModeBase) };
     }
 }
--- Assets/Mode/Mode Base/Difficulty/NoFailModeBase.cs
+++ Assets/Mode/Mode Base/Difficulty/NoFailModeBase.cs
@@ -1,4 +1,6 @@
 using SCKRM.Renderer;
+using SDJK.Mode.Automatic;
+using System;
 
 namespace SDJK.Mode.Difficulty
 {
@@ -10,5 +12,7 @@ namespace SDJK.Mode.Difficulty
         public override NameSpacePathPair info { get; } = "sdjk:mode.difficulty.no_fail.info";
 
         public override NameSpaceIndexTypePathPair icon { get; } = "sdjk:0:mode/difficulty_no_fail";
+
+        public override Type[] incompatibleModes { get; } = new Type[] { typeof(SuddenDeathModeBase), typeof(PerfectModeBase), typeof(AutoModeBase) };
     }
 }
