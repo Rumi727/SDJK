6032440a5d80c444f72487b92983d9311cc8337b
Simsimhan Chobo <jun32820@gmail.com>
2023-07-16 22:53:24 +0900

    Super Hexagon Ruleset 상태에선 특정 모드 이름과 설명이 약간 다르게 나오도록 변경

--- Assets/Mode/Ruleset/Super Hexagon/Converter/SuperHexagonKeyCountMode.cs
+++ Assets/Mode/Ruleset/Super Hexagon/Converter/SuperHexagonKeyCountMode.cs
@@ -1,3 +1,4 @@
+using SCKRM.Renderer;
 using SDJK.Mode.Converter;
 
 namespace SDJK.Mode.Ruleset.SuperHexagon.Converter
@@ -5,5 +6,8 @@ namespace SDJK.Mode.Ruleset.SuperHexagon.Converter
     public sealed class SuperHexagonKeyCountMode : KeyCountModeBase
     {
         public override string targetRuleset { get; } = "super_hexagon";
+
+        public override NameSpacePathReplacePair displayName { get; } = "sdjk:mode.converter.key_count.super_hexagon";
+        public override NameSpacePathPair info { get; } = "sdjk:mode.converter.key_count.super_hexagon.info";
     }
 }
--- Assets/Mode/Ruleset/Super Hexagon/Difficulty/SuperHexagonSuddenDeathMode.cs
+++ Assets/Mode/Ruleset/Super Hexagon/Difficulty/SuperHexagonSuddenDeathMode.cs
@@ -1,3 +1,4 @@
+using SCKRM.Renderer;
 using SDJK.Mode.Difficulty;
 
 namespace SDJK.Mode.Ruleset.SuperHexagon.Difficulty
@@ -5,5 +6,7 @@ namespace SDJK.Mode.Ruleset.SuperHexagon.Difficulty
     public sealed class SuperHexagonSuddenDeathMode : SuddenDeathModeBase
     {
         public override string targetRuleset { get; } = "super_hexagon";
+
+        public override NameSpacePathPair info { get; } = "sdjk:mode.difficulty.sudden_death.super_hexagon.info";
     }
 }
