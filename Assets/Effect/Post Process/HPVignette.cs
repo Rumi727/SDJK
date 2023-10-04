4918fc6fbaa9b89eadc8e1144330488907c76170
Simsimhan Chobo <jun32820@gmail.com>
2023-10-04 21:40:42 +0900

    HP 비네팅 수정

--- Assets/Effect/Post Process/HPVignette.cs
+++ Assets/Effect/Post Process/HPVignette.cs
@@ -13,7 +13,7 @@ namespace SDJK.Effect.PostProcessing
         float lerpValue = 0;
         protected override void RealUpdate()
         {
-            float value = 1 - 0f.InverseLerp(maxHp * 0.5f, hp);
+            float value = (2 - 0f.InverseLerpUnclamped(maxHp * 0.35f, hp)).Clamp(0, 1);
             lerpValue = lerpValue.Lerp(value, 0.0625f * RhythmManager.bpmFpsDeltaTime);
 
             Vignette vignette = profile.GetSetting<Vignette>();
